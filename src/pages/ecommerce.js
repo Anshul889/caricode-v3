d81a1c7081a27c0a68536ce3d1a7d92cb50e90e3
Anshul889 <anshulmanaktala@gmail.com>
2021-09-02 17:17:28 +0530

    ecommerce styled components

--- src/pages/ecommerce.js
+++ src/pages/ecommerce.js
@@ -5,178 +5,102 @@ import Image from 'gatsby-image'
 import EcommerceHero from '../components/Banners/Ecommerce/EcommerceHero'
 import EcommerceTwoSelect from '../components/EcommerceSelect/EcommerceTwoSelect'
 import Layout from '../components/Layout/Layout'
-
-const getImages = graphql`
-  {
-    image: file(relativePath: { eq: "handshake.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    fashion: file(relativePath: { eq: "fashion.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    jewellery: file(relativePath: { eq: "jewellery.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    beauty: file(relativePath: { eq: "beauty.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    food: file(relativePath: { eq: "food.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    home: file(relativePath: { eq: "home.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    sports: file(relativePath: { eq: "sports.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
+import {
+  Card,
+  Cards,
+  Carousel,
+  CarouselContainer,
+  CarouselItem,
+  Number,
+} from '../styles/ecommerce-style'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Ecommerce = () => {
-  const data = useStaticQuery(getImages)
   return (
     <Layout>
-      <div className={styles.ecombody}>
-        <EcommerceHero />
-        <div className={styles.carouselcontainer}>
-          <h4>
-            Deliver an amazing ecommerce experience to your customers. A small
-            selection of our work
-          </h4>
-          <div className={styles.ecomcarousel}>
-            <div className={styles.carouselitem}>
-              <a
-                href="https://luxxette.web.app"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  fluid={data.fashion.childImageSharp.fluid}
-                  style={{ borderRadius: '4px' }}
-                  className={styles.carouselimage}
-                />
-              </a>
-              <p>Fashion</p>
-            </div>
-            <div className={styles.carouselitem}>
-              <a
-                href="https://hola-bella-adbaa.web.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  fluid={data.sports.childImageSharp.fluid}
-                  style={{ borderRadius: '4px' }}
-                  className={styles.carouselimage}
-                />
-              </a>
-              <p>Jewellery</p>
-            </div>
-            <div className={styles.carouselitem}>
-              <a
-                href="https://canopy-caricode.web.app"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  fluid={data.jewellery.childImageSharp.fluid}
-                  style={{ borderRadius: '4px' }}
-                  className={styles.carouselimage}
-                />
-              </a>
-              <p>Jewellery</p>
-            </div>
-            <div className={styles.carouselitem}>
-              <a
-                href="https://beauty-caricode.web.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  fluid={data.beauty.childImageSharp.fluid}
-                  style={{ borderRadius: '4px' }}
-                  className={styles.carouselimage}
-                />
-              </a>
-              <p>Beauty</p>
-            </div>
-            <div className={styles.carouselitem}>
-              <Image
-                fluid={data.food.childImageSharp.fluid}
-                style={{ borderRadius: '4px' }}
-                className={styles.carouselimage}
-              />
-              <p>Food</p>
-            </div>
-            <div className={styles.carouselitem}>
-              <Image
-                fluid={data.home.childImageSharp.fluid}
-                style={{ borderRadius: '4px' }}
-                className={styles.carouselimage}
-              />
-              <p>Home</p>
-            </div>
-            <div className={styles.nothing}></div>
-          </div>
-        </div>
-        <div className={styles.cardsection}>
-          <h4>Get started with 3 easy steps</h4>
-          <div className={styles.card}>
-            <div className={styles.number}>1</div>
-            <h4>Base Features</h4>
-            <p>
-              Starter pack with base features including a responsive website to
-              feature unlimited products with payment support
-            </p>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.number}>2</div>
-            <h4>Choose Additional Features</h4>
-            <p>
-              To take your ecommerce app to the next level choose from a range
-              from of features including Email Marketing, Search Engine
-              Optimization and Social
-            </p>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.number}>3</div>
-            <h4>Hit Submit</h4>
-            <p>
-              Confirm your order and we take care of the rest, providing end to
-              end support
-            </p>
-          </div>
-        </div>
-        <EcommerceTwoSelect />
-      </div>
+      <EcommerceHero />
+      <CarouselContainer>
+        <h4>
+          Deliver an amazing ecommerce experience to your customers. A small
+          selection of our work
+        </h4>
+        <Carousel>
+          <CarouselItem>
+            <a href="https://luxxette.web.app" target="_blank" rel="noreferrer">
+              <StaticImage src="../images/fashion.jpg"></StaticImage>
+            </a>
+            <p>Fashion</p>
+          </CarouselItem>
+          <CarouselItem>
+            <a
+              href="https://hola-bella-adbaa.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StaticImage src="../images/sports.jpg"></StaticImage>
+            </a>
+            <p>Jewellery</p>
+          </CarouselItem>
+          <CarouselItem>
+            <a
+              href="https://canopy-caricode.web.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StaticImage src="../images/jewellery.jpg"></StaticImage>
+            </a>
+            <p>Jewellery</p>
+          </CarouselItem>
+          <CarouselItem>
+            <a
+              href="https://beauty-caricode.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StaticImage src="../images/beauty.jpg"></StaticImage>
+            </a>
+            <p>Beauty</p>
+          </CarouselItem>
+          <CarouselItem>
+            <StaticImage src="../images/food.jpg"></StaticImage>
+            <p>Food</p>
+          </CarouselItem>
+          <CarouselItem>
+            <StaticImage src="../images/home.jpg"></StaticImage>
+            <p>Home</p>
+          </CarouselItem>
+          <div className={styles.nothing}></div>
+        </Carousel>
+      </CarouselContainer>
+      <Cards>
+        <h4>Get started with 3 easy steps</h4>
+        <Card>
+          <Number>1</Number>
+          <h4>Base Features</h4>
+          <p>
+            Starter pack with base features including a responsive website to
+            feature unlimited products with payment support
+          </p>
+        </Card>
+        <Card>
+          <Number>2</Number>
+          <h4>Choose Additional Features</h4>
+          <p>
+            To take your ecommerce app to the next level choose from a range
+            from of features including Email Marketing, Search Engine
+            Optimization and Social
+          </p>
+        </Card>
+        <Card>
+          <Number>3</Number>
+          <h4>Hit Submit</h4>
+          <p>
+            Confirm your order and we take care of the rest, providing end to
+            end support
+          </p>
+        </Card>
+      </Cards>
+      <EcommerceTwoSelect />
     </Layout>
   )
 }
---
+++ src/styles/ecommerce-style.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components'
+
+export const CarouselContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 45px 0;
+  clip-path: polygon(0 0, 100% 4%, 100% 100%, 0 95%);
+  background-color: rgb(94, 173, 187);
+  transform: translateY(-73px);
+  color: white;
+  h4 {
+    padding-top: 15px;
+    margin: 0 auto;
+    width: 90%;
+    max-width: 1200px;
+    margin-bottom: 28px;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 55px 0;
+    transform: translateY(-80px);
+    clip-path: polygon(0 0, 100% 7%, 100% 100%, 0 93%);
+    h4 {
+      margin-bottom: 38px;
+    }
+  }
+`
+
+export const Carousel = styled.div`
+  display: grid;
+  grid-template-columns: 66% 66% 66% 66% 66% 66% 5px;
+  overflow: scroll;
+  grid-gap: 30px;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+    max-width: 1080px;
+    margin: 0 auto;
+    text-align: center;
+    width: 90%;
+    overflow: visible;
+    transform: translateX(0);
+  }
+`
+
+export const CarouselItem = styled.div`
+  transform: translateX(5%);
+  .gatsby-image-wrapper {
+    border-radius: 4px;
+  }
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  @media screen and (min-width: 768px) {
+    .gatsby-image-wrapper:hover {
+      transition: transform 0.3s ease-in-out;
+      transform: scale(1.1);
+    }
+  }
+`
+
+export const Cards = styled.div`
+  transform: translateY(-23px);
+  @media screen and (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto 470px;
+    max-width: 1080px;
+    margin: 0 auto;
+    padding-bottom: 30px;
+  }
+`
+
+export const Card = styled.div`
+  width: 90%;
+  margin: 38px auto;
+  background-color: white;
+  padding-bottom: 10px;
+  box-shadow: 0 1em 5em rgba(0, 0, 0, 0.05);
+  padding-top: 10px;
+  p {
+    width: 90%;
+    margin: 15px auto;
+    text-align: center;
+  }
+  h4 {
+    padding-top: 20px;
+  }
+  @media screen and (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 75px 100px 205px;
+    p {
+      margin: 35px auto;
+    }
+  }
+`
+
+export const Number = styled.div``
