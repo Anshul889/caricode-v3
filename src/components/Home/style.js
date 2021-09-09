caedd7d7461f518f502a38e8b7a6e74f0a969d36
Anshul889 <anshulmanaktala@gmail.com>
2021-09-09 17:25:43 +0530

    medium desktop width home page

--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -8,6 +8,8 @@ export const Container = styled.div`
   background-color: #292728;
   color: white;
   @media screen and (min-width: 600px) {
+  }
+  @media screen and (min-width: 1200px) {
     padding-top: 70px;
     display: grid;
   }
@@ -18,12 +20,15 @@ export const Grid = styled.div`
   grid-template-columns: 1fr;
   row-gap: 20px;
   padding-top: 20px;
-  @media screen and (min-width: 600px) {
+  @media screen and (min-width: 800px) {
+    transform: translateY(50px);
     grid-template-columns: repeat(12, 1fr);
+    padding-top: 0px;
+  }
+  @media screen and (min-width: 1200px) {
     place-self: center;
     width: 95%;
-    padding-top: 0px;
-    transform: translateX(50px)
+    transform: translateX(50px);
   }
   @media screen and (min-width: 1600px) {
     height: auto;
@@ -37,8 +42,12 @@ export const Animation = styled.div`
   @media screen and (min-width: 600px) {
     height: 600px;
     grid-column: 5 / span 9;
-    grid-row: 1/ span 1;
+    grid-row: 1 / span 1;
+    padding-top: 0px;
+  }
+  @media screen and (min-width: 1200px) {
     transform: translateY(-80px);
+    padding-top: 10px;
   }
 
   @media screen and (min-width: 1600px) {
@@ -56,8 +65,11 @@ export const BannerText = styled.div`
     display: grid;
     place-content: center;
     text-align: start;
-    grid-column: 1 / span 5;
-    grid-row: 1/ span 1
+    grid-column: 1 / span 6;
+    grid-row: 1 / span 1;
+  }
+  @media screen and (min-width: 1200px) {
+    grid-column: 1 / span 6;
   }
 `
 
