bc9fef88be36545acc42efd841a6c2b4592cf9eb
Anshul889 <anshulmanaktala@gmail.com>
2023-07-06 12:44:50 +0530

    radio options  complete

--- src/components/UiComponents/UiComponents.jsx
+++ src/components/UiComponents/UiComponents.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as styles from './UiComponents.module.css'
-import { Popover } from '@headlessui/react'
+import { Popover, RadioGroup } from '@headlessui/react'
 
 const UiComponents = () => {
+  const [plan, setPlan] = useState('startup')
   return (
     <div>
       <h2 className={styles.heading}>Custom UI Elements</h2>
@@ -24,6 +25,46 @@ const UiComponents = () => {
               </div>
             </Popover.Panel>
           </Popover>
+        </div>
+      </div>
+      <div className={styles.radiocontainer}>
+        <div className={styles.inner}>
+          <RadioGroup value={plan} onChange={setPlan}>
+            <div className={styles.radioheading}>Select your Plan</div>
+            <RadioGroup.Option value="startup">
+              {({ checked }) => (
+                <div className={checked ? styles.active : styles.inactive}>
+                  <div className={styles.radiooption}>Startup</div>
+                  <div className={styles.radiodescription}>
+                    12GB/8 CPUs 160 GB SSD disk
+                  </div>
+                  {checked && <div className={styles.tick}>&#10003;</div>}
+                </div>
+              )}
+            </RadioGroup.Option>
+            <RadioGroup.Option value="business">
+              {({ checked }) => (
+                <div className={checked ? styles.active : styles.inactive}>
+                  <div className={styles.radiooption}>Business</div>
+                  <div className={styles.radiodescription}>
+                    16GB/8 CPUs 512 GB SSD disk
+                  </div>
+                  {checked && <div className={styles.tick}>&#10003;</div>}
+                </div>
+              )}
+            </RadioGroup.Option>
+            <RadioGroup.Option value="enterprise">
+              {({ checked }) => (
+                <div className={checked ? styles.active : styles.inactive}>
+                  <div className={styles.radiooption}>Enterprise</div>
+                  <div className={styles.radiodescription}>
+                    32GB/8 CPUs 1 TB SSD disk
+                  </div>
+                  {checked && <div className={styles.tick}>&#10003;</div>}
+                </div>
+              )}
+            </RadioGroup.Option>
+          </RadioGroup>
         </div>
       </div>
     </div>
